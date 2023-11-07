305b9a86135d8e2847bf1104326d41f8a5787479
talha <talhaaly103@gmail.com>
2023-11-07 12:47:01 +0500

    Play Pause-By Js

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ function play () {
     if (video.paused){
         video.play();
         butn.style.background = "url(play.png) no-repeat"
+        butn.style.background = "red"
     }else {
         video.pause()
         butn.style.background = "url(pause.png) no-repeat"
+        butn.style.background = "black"
     }
 }
